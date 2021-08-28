81edee3882f3f7138930a64304ca149dc5b2d013
Daniel Figueroa <danfigueroa81@gmail.com>
2021-08-27 22:27:49 -0400

    add toggle ability to notification buttons and make selections persist

--- WNHL-App/NotificationsTableViewController.swift
+++ WNHL-App/NotificationsTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class NotificationsTableViewController: UITableViewController {
-
+    let defaults = UserDefaults.standard
     @IBOutlet var NotificationsTableView: UITableView!
     var reuseIdentifier = "notificationTableCell"
     var teams = ["Atlas Steelers", "Townline Tunnelers", "Crown Room Kings", "Dain City Dusters", "Lincoln Street Legends","Merritt Islanders"]
@@ -26,15 +26,29 @@ class NotificationsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             // Segue to the second view controller
-        print("bababooey")
-            // self.performSegue(withIdentifier: "playersSegue", sender: self)
+        let indexPath = NotificationsTableView.indexPathForSelectedRow
+        let cell = NotificationsTableView.cellForRow(at: indexPath!) as! NotificationTableViewCell
+        let rowNumber:String = String(indexPath!.row)
+        if cell.checkButton.isSelected == false{
+            cell.checkButton.isSelected = true
+            defaults.setValue(true, forKey: "checkButton" + rowNumber)
+        }
+        else{
+            cell.checkButton.isSelected = false
+            defaults.setValue(false, forKey: "checkButton" + rowNumber)
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = NotificationsTableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationTableViewCell
+        let rowNumber:String = String(indexPath.row)
+        cell.checkButton.isSelected = defaults.bool(forKey: "checkButton" + rowNumber)
         cell.teamLabel.text = teams[indexPath.row]
         cell.teamLabel.textColor = UIColor.white
         cell.teamLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        cell.selectionStyle = .none
+        cell.checkButton.setBackgroundImage(UIImage(systemName: "square"), for: .normal)
+        cell.checkButton.setBackgroundImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
         cell.backgroundColor = UIColor.systemOrange
         return cell
     }
