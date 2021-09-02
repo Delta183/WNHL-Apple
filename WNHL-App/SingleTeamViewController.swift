e57df6095b37fd1d8c984b16f6533c6a8cc7783d
Daniel Figueroa <danfigueroa81@gmail.com>
2021-09-01 23:25:43 -0400

    Add custom colour background to the single team views. Subject to change once Sawyer finalizes desired colours.

--- WNHL-App/SingleTeamViewController.swift
+++ WNHL-App/SingleTeamViewController.swift
@@ -24,10 +24,36 @@ class SingleTeamViewController: UIViewController {
         button?.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
         teamNameLabel.text = teamNameString
         teamLogo.image = UIImage(named: getImageNameFromTeamName(teamName: teamNameString))
+        self.view.backgroundColor = getColorFromTeamId(teamNameString: teamNameString)
     }
     
     @objc func buttonClicked() {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+func getColorFromTeamId(teamNameString: String) -> UIColor{
+    if teamNameString.caseInsensitiveCompare("Atlas Steelers")  == ComparisonResult.orderedSame{
+        return UIColor(red: 200.0/255.0, green: 10.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    }
+    else if teamNameString.caseInsensitiveCompare("Townline Tunnelers") == ComparisonResult.orderedSame{
+        return UIColor.systemBlue
+    }
+    else if teamNameString.caseInsensitiveCompare("Crown Room Kings") == ComparisonResult.orderedSame{
+        return UIColor.black
+    }
+    else if teamNameString.caseInsensitiveCompare("Dain City Dusters") == ComparisonResult.orderedSame{
+        return UIColor.red
+    }
+    else if teamNameString.caseInsensitiveCompare("Lincoln Street Legends") == ComparisonResult.orderedSame{
+        // This will allow the creation of custom colours through changing the numerators
+        return UIColor(red: 10.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    }
+    else if teamNameString.caseInsensitiveCompare("Merritt Islanders") == ComparisonResult.orderedSame{
+        return UIColor.systemGray2
+    }
+    else{
+        return UIColor.systemOrange
     }
 }
 
