1efea8b79a490ca95715e9ad0cfb8cba79394f48
Daniel Figueroa <danfigueroa81@gmail.com>
2021-09-13 16:20:30 -0400

    create outlets for schedule labels. Able to dynamically change now

--- WNHL-App/ViewControllers/HomeViewController.swift
+++ WNHL-App/ViewControllers/HomeViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var yearsLabel: UITextField!
+    @IBOutlet weak var scheduleTitleLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        yearsLabel.text = "1999-2000"
+        scheduleTitleLabel.text = "Extreme Season"
         // Do any additional setup after loading the view.
     }
 }
